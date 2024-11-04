5c8d253bc6651702e1b6caf6a8d275bb96dba77c
Timur <arturtimur201998@gmail.com>
2024-11-04 18:42:18 +0500

    d

---
+++ creditcard/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := "4400430180300029"
+	if Validate(number) == true {
+		fmt.Println("isValid")
+	} else {
+		fmt.Println("NoValid")
+	}
+}
+
+func Validate(number string) bool {
+	sum := 0
+	isSecomd := false
+
+	for i := len(number) - 1; i >= 0; i-- {
+		d := int(number[i] - '0')
+
+		if isSecomd {
+			d += 2
+			if d > 9 {
+				d -= 9
+			}
+		}
+
+		sum += d
+		isSecomd = !isSecomd
+	}
+
+	return sum%10 == 0
+}
